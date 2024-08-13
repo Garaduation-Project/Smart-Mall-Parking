55b62806b404a9da4e63d1272dea066f13334237
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2024-08-13 21:14:38 +0300

    Update reserve_api_model.dart

--- lib/models/reserve_api_model.dart
+++ lib/models/reserve_api_model.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:parking/models/login_api_model.dart';
